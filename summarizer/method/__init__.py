95d1098f953b48aec2fdc13b2018b4353c438d28
Sangjun Park <cosmoquester@gmail.com>
2021-12-18 00:37:47 +0900

    feat: Add R3F method

--- summarizer/method/__init__.py
+++ summarizer/method/__init__.py
@@ -1,4 +1,5 @@
 from .default import DefaultModule
+from .r3f import R3FModule
 from .rdrop import RDropModule
 
-__all__ = ["DefaultModule", "RDropModule"]
+__all__ = ["DefaultModule", "R3FModule", "RDropModule"]
